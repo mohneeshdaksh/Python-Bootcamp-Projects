c89741741a33e31f6c05b53f755c4f5e192d9f84
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-11-19 23:20:38 +0530

    Modify code

--- Randomization and Lists/Treasure Map/Treasure Map.py
+++ Randomization and Lists/Treasure Map/Treasure Map.py
@@ -1,11 +1,19 @@
+# Basic program to hide your treasure using Lists.
+
 line1 = ["⬜️","️⬜️","️⬜️"]
 line2 = ["⬜️","⬜️","️⬜️"]
 line3 = ["⬜️️","⬜️️","⬜️️"]
+
 map = [line1, line2, line3]
-print("Hiding your treasure! X marks the spot.")
+
+print("\nHiding your treasure! X marks the spot.")
+
 position = input().lower()
+
 reference_list = ["a", "b", "c"]
-second_index = reference_list.index(position[0])
+
 first_index = int(position[1]) - 1
+second_index = reference_list.index(position[0])
 map[first_index][second_index] = "X"
+
 print(f"{line1}\n{line2}\n{line3}")
