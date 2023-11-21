097ae4c90aed6c0ccaaef1ee90540bcb89939740
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-11-21 23:17:50 +0530

    Modify code

--- Capstone Projects/Blackjack/Blackjack.py
+++ Capstone Projects/Blackjack/Blackjack.py
@@ -5,15 +5,6 @@ from art import logo
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def blackjack_check(your_sum, computer_sum):
-    if your_sum == 21:
-        return "Win"
-    elif computer_sum == 21:
-        return "Lose"
-    else:
-        return "Resume"
-
-
 
 def result(player_total, dealer_total):
     if player_total < 22 and dealer_total < 22:
@@ -41,65 +32,73 @@ def ace_check(player_set, player_sum, dealer_set, dealer_sum):
                 dealer_set[num] = 1
 
 
-
-
-
 def blackjack():
     os.system('clear')
     print(logo)
-    your_cards = []
+    player_cards = []
     computer_cards = []
     for card in range(0,2):
-        your_cards.append(random.choice(cards))
+        player_cards.append(random.choice(cards))
         computer_cards.append(random.choice(cards))
     
-    your_cards_sum = 0
-    for number in your_cards:
-        your_cards_sum += number
+    player_sum = 0
+    for number in player_cards:
+        player_sum += number
     
-    
-    computer_cards_sum = 0
+    computer_sum = 0
     for num in computer_cards:
-        computer_cards_sum += num
+        computer_sum += num
     
-    print(f"    Your cards: {your_cards}, current score: {your_cards_sum}")
+    print(f"    Your cards: {player_cards}, current score: {player_sum}")
     print(f"    Computer's first card: {computer_cards[0]}")
 
-    # Checking if it is a blackjack or not!
-    check = blackjack_check(your_sum=your_cards_sum, computer_sum=computer_cards_sum)
 
-    if check == "Win":
-        print("You win 😃")
-    elif check == "Lose":
-        print("You lose 😤")
-    elif check == "Resume":
+    if player_sum == 21:
+        print("You win with a blackjack 😃")
+    elif computer_sum == 21:
+        print("Lose, opponent got blackjack 😱")
+    elif player_sum == 21 and computer_sum == 21:
+        print("Draw 🙃")
+    else:
+        if player_sum > 21:
+            if 11 in player_cards:
+                for value in player_cards:
+                    if player_cards[value] == 11:
+                        player_cards[value] = 1
+                        
+            else:
+                print("You went over. You lose 😤")
+        else:
+            # ask if wants to pick another card
+
+
         pick_next_card = True
         while pick_next_card:
             another_card = input("Type 'y' to get another card or type 'n' to pass: ")
             if another_card == 'y':
                 your_next_card = random.choice(cards)
-                your_cards.append(your_next_card)
-                your_cards_sum += your_next_card
-                print(f"    Your cards: {your_cards}, current score: {your_cards_sum}")
+                player_cards.append(your_next_card)
+                player_sum += your_next_card
+                print(f"    Your cards: {player_cards}, current score: {player_sum}")
                 print(f"    Computer's first card: {computer_cards[0]}")
-                if your_cards_sum > 21:
+                if player_sum > 21:
                     pick_next_card = False
             else:
                 pick_next_card = False
 
         pick_more = True
         while pick_more:
-            if computer_cards_sum < 16:
+            if computer_sum < 16:
                 computer_next_card = random.choice(cards)
                 computer_cards.append(computer_next_card)
-                computer_cards_sum += computer_next_card
+                computer_sum += computer_next_card
             else:
                 pick_more = False
 
-        print(f"    Your cards: {your_cards}, final score: {your_cards_sum}")
-        print(f"    Computer cards: {computer_cards}, final score: {computer_cards_sum}")
+        print(f"    Your cards: {player_cards}, final score: {player_sum}")
+        print(f"    Computer cards: {computer_cards}, final score: {computer_sum}")
         
-        result(player_total=your_cards_sum, dealer_total=computer_cards_sum)
+        result(player_total=player_sum, dealer_total=computer_sum)
         
 
 
@@ -115,3 +114,19 @@ while play_blackjack:
         play_blackjack = False
         os.system('clear')
         print("Goodbye!")
+
+
+
+
+# def blackjack_check(your_sum, computer_sum):
+#     if your_sum == 21:
+#         return "Win"
+#     elif computer_sum == 21:
+#         return "Lose"
+#     elif your_sum == computer_sum:
+#         return "Draw"
+#     else:
+#         return "Resume"
+
+# Checking if it is a blackjack or not!
+# check = blackjack_check(your_sum=your_cards_sum, computer_sum=computer_cards_sum)
