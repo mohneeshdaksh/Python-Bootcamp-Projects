3d64cd8e7277c96c475bc0211890ae1c03636492
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-11-29 11:02:32 +0530

    Improve code

--- Data Types and F String/tip_calculator.py
+++ Data Types and F String/tip_calculator.py
@@ -1,7 +1,8 @@
-#Tip Calculator to calulate bill share per person including the desired tip.
+'''Tip Calculator to calulate bill share per person including the desired tip'''
+
 print("Welcome to the Tip Calculator!")
 total_bill = input("What was the total bill? $")
-tip_percent = input("What percentage tip would you like to give? For ex: 10, 12 or 15? (Value should be from 1 to 100 without the '%' sign) ")
+tip_percent = input("What percentage tip would you like to give? Eg. 10, 15, etc")
 total_people = input("How many people are going to split the bill? ")
 per_person_share = float(total_bill) / float(total_people)
 percent_increase = 1 + (float(tip_percent)/100)
