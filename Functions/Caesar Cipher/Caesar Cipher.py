438d7f3a91947fd61a56d12f95bcab7a27a414ad
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-12-02 23:52:25 +0545

    Improve code

--- Functions/Caesar Cipher/Caesar Cipher.py
+++ Functions/Caesar Cipher/Caesar Cipher.py
@@ -1,8 +1,9 @@
 '''Caesar Cipher'''
 
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-from art import logo
 print(logo)
 
 def caesar(start_text, shift_amount, cipher_direction):
