8528e32916b0262a1ae1be542d4e4a1ca1a59c43
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-12-02 23:43:33 +0545

    Improve code

--- Functions/Caesar Cipher/Caesar Cipher.py
+++ Functions/Caesar Cipher/Caesar Cipher.py
@@ -1,3 +1,4 @@
+'''Caesar Cipher'''
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -25,7 +26,7 @@ while loop_execution:
     shift = int(input("Type the shift number:\n"))
     shift = shift % 26
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-    
+
     restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
     if restart == "no":
         loop_execution = False
