5c0f6b8bc679d3767eb446ea883c7f5b076e67d6
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-12-04 20:46:40 +0530

    Initial commit

---
+++ Functions/Prime Number Checker/Prime Number Checker.py
@@ -0,0 +1,17 @@
+def prime_checker(number):
+    if number > 1:
+        count = 0
+        for divisor in range(2,number):
+            modulo = number % divisor
+            if modulo == 0:
+                count += 1
+        if count > 0:
+            print("It's not a prime number.")
+        else:
+            print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+            
+print("Welcome to the Prime Number Checker!")
+n = int(input("Enter a number: "))
+prime_checker(number=n)
