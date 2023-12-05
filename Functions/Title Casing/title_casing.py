e267501eb0d4f57a7b58b7628c8a830bfc215076
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-12-05 23:30:05 +0530

    Improve code

--- Functions/Title Casing/title_casing.py
+++ Functions/Title Casing/title_casing.py
@@ -1,7 +1,8 @@
+'''Title Casing'''
 from art import logo
 def format_name(first_name, last_name):
-    """Take a first and last name and format it to return 
-    the title case version of the name."""
+    '''Take a first and last name and format it to return 
+    the title case version of the name.'''
     if first_name == "" or last_name == "":
         return "Invalid entry!"
     else:
