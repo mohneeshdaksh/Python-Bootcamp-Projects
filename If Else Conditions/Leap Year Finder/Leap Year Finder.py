f00b9c5b960c9dcb1ab1d1d0ac5c4574dc54a873
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-12-07 00:08:37 +0530

    Improve code

--- If Else Conditions/Leap Year Finder/Leap Year Finder.py
+++ If Else Conditions/Leap Year Finder/Leap Year Finder.py
@@ -1,5 +1,4 @@
-#Consufed in finding which year is a leap year? 
-#No worries! Just type the year and I'll let you know whether it is a leap year or not!
+'''Leap Year Finder'''
 print("Leap Year Finder!")
 year = int(input("Please provide the year - "))
 if year % 4 == 0:
