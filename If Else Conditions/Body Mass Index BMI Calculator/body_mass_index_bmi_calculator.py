54174e3add6fcb15eaa348b823e5d98d5dae6ba0
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-12-08 22:15:59 +0530

    Improve code

--- If Else Conditions/Body Mass Index BMI Calculator/body_mass_index_bmi_calculator.py
+++ If Else Conditions/Body Mass Index BMI Calculator/body_mass_index_bmi_calculator.py
@@ -11,10 +11,10 @@ BMI = weight / height**2
 if BMI < 18.5:
     print(f"Your BMI is {BMI}, you are underweight.")
 elif BMI < 25:
-        print(f"Your BMI is {BMI}, you have a normal weight.")
+    print(f"Your BMI is {BMI}, you have a normal weight.")
 elif BMI < 30:
-        print(f"Your BMI is {BMI}, you are slightly overweight.")
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
 elif BMI < 35:
-        print(f"Your BMI is {BMI}, you are obese.")
+    print(f"Your BMI is {BMI}, you are obese.")
 else:
     print(f"Your BMI is {BMI}, you are clinically obese")
