2b3189a4d3f549c12bb987766a94ee727e7e0f9e
Mohneesh Daksh <mohneeshdaksh@outlook.com>
2023-12-12 22:19:52 +0530

    Improve code

--- If Else Conditions/Python Pizza Deliveries/python_pizza_deliveries.py
+++ If Else Conditions/Python Pizza Deliveries/python_pizza_deliveries.py
@@ -5,7 +5,9 @@ print("Thank you for choosing Python Pizza Deliveries!")
 size = input("Which size of pizza would you like to have? S, M or L? ")
 add_pepperoni = input("Do you want pepperoni on it? Y or N? ")
 extra_cheese = input("Would you like to have an extra cheese on your pizza? Y or N? ")
+
 bill = 0
+
 if size == "S":
     bill += 15
 elif size == "M":
@@ -14,6 +16,7 @@ elif size == "L":
     bill += 25
 else:
     print("Invalid entry! :(")
+
 if add_pepperoni == "Y":
     if size == "S":
         bill += 2
